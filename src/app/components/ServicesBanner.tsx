c69dcd9fc1879e25fbc917782ca6cebb6a307e5b
BasTheBoarBoi <tripidokintasuwan@gmail.com>
2025-06-08 17:06:39 +0700

    Fix all the problem

--- src/app/components/ServicesBanner.tsx
+++ src/app/components/ServicesBanner.tsx
@@ -1,49 +1,49 @@
 "use client";
-import React, { useEffect, useState } from "react";
+// import React, { useEffect, useState } from "react";
 import TarotCard from "./TarotCard";
 import Image from "next/image";
 //Understanding the tarotCardSrc array//
-const tarotCardSrc = [
-  {
-    imageSrc: "/servicetarotmark1.png",
-    nameCard: "Daily Horoscope Readings",
-    describeCard:
-      "Short, engaging predictions for love, career, health, and personal growth Users can select their zodiac sign to view personalized readings.",
-    className: `z-1 transition-all duration-300 md:mt-20 md:rotate-[-20deg] lg:mt-30 lg:rotate-[-30deg] xl:mt-40 xl:rotate-[-40deg]`,
-  },
-  {
-    imageSrc: "/servicetarotmark2.png",
-    nameCard: "Natal Birth Chart Analysis",
-    describeCard:
-      "Explain the significance of the Sun, Moon, and Rising signs, along with planetary placements Interactive birth chart generator where users input their details.",
-    className: `z-2 -ml-[370px] transition-all duration-300 md:-ml-[300px] lg:-ml-[170px] xl:-ml-[80px] 2xl:-ml-[0px]`,
-  },
-  {
-    imageSrc: "/servicetarotmark3.png",
-    nameCard: "Couples Compatibility Report",
-    describeCard:
-      "Insights into emotional, intellectual, and physical compatibility. Users enter their birth details to receive a compatibility score and analysis.",
-    className: `z-3 -ml-[370px] transition-all duration-300 md:-ml-[300px] md:mt-20 md:rotate-[20deg] lg:-ml-[170px] lg:mt-30 lg:rotate-[30deg] xl:-ml-[80px] xl:mt-40 xl:rotate-[40deg] 2xl:-ml-[0px]`,
-  },
-];
+// const tarotCardSrc = [
+//   {
+//     imageSrc: "/servicetarotmark1.png",
+//     nameCard: "Daily Horoscope Readings",
+//     describeCard:
+//       "Short, engaging predictions for love, career, health, and personal growth Users can select their zodiac sign to view personalized readings.",
+//     className: `z-1 transition-all duration-300 md:mt-20 md:rotate-[-20deg] lg:mt-30 lg:rotate-[-30deg] xl:mt-40 xl:rotate-[-40deg]`,
+//   },
+//   {
+//     imageSrc: "/servicetarotmark2.png",
+//     nameCard: "Natal Birth Chart Analysis",
+//     describeCard:
+//       "Explain the significance of the Sun, Moon, and Rising signs, along with planetary placements Interactive birth chart generator where users input their details.",
+//     className: `z-2 -ml-[370px] transition-all duration-300 md:-ml-[300px] lg:-ml-[170px] xl:-ml-[80px] 2xl:-ml-[0px]`,
+//   },
+//   {
+//     imageSrc: "/servicetarotmark3.png",
+//     nameCard: "Couples Compatibility Report",
+//     describeCard:
+//       "Insights into emotional, intellectual, and physical compatibility. Users enter their birth details to receive a compatibility score and analysis.",
+//     className: `z-3 -ml-[370px] transition-all duration-300 md:-ml-[300px] md:mt-20 md:rotate-[20deg] lg:-ml-[170px] lg:mt-30 lg:rotate-[30deg] xl:-ml-[80px] xl:mt-40 xl:rotate-[40deg] 2xl:-ml-[0px]`,
+//   },
+// ];
 //Digest section//
 export default function ServicesBanner() {
-  const [currentIndex, setCurrentIndex] = useState(0);
+  // const [currentIndex, setCurrentIndex] = useState(0);
 
-  useEffect(() => {
-    const handleScroll = () => {
-      const scrollPosition = window.scrollY;
-      const newIndex = Math.min(tarotCardSrc.length - 1, Math.floor(scrollPosition / 2000));
-      setCurrentIndex(newIndex);
-    };
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, [currentIndex]);
+  // useEffect(() => {
+  //   const handleScroll = () => {
+  //     const scrollPosition = window.scrollY;
+  //     const newIndex = Math.min(tarotCardSrc.length - 1, Math.floor(scrollPosition / 500));
+  //     setCurrentIndex(newIndex);
+  //   };
+  //   window.addEventListener("scroll", handleScroll);
+  //   return () => {
+  //     window.removeEventListener("scroll", handleScroll);
+  //   };
+  // }, [currentIndex]);
 
   return (
-    <div className="snap-center snap-always">
+    <div className="snap-start snap-always">
       <div className="flex flex-col items-center justify-center bg-[url('/servicebg.png')] bg-cover bg-center text-white text-center p-8">
         <h1 className="transition-all duration-300 text-[#ffd700] font-bold text-4xl md:text-5xl mt-10 mb-1">
           Our Services
@@ -51,13 +51,13 @@ export default function ServicesBanner() {
         <h2 className="transition-all duration-300 text-[#ffd700] font-normal text-1xl md:text-2xl mb-5 ">
           Explore Our Personalized Astrology Offerings
         </h2>
-        <div className="flex items-center justify-center">
+        {/* <div className="flex items-center justify-center">
           {tarotCardSrc.map((card, index) => (
             <div
               key={index}
               className={`${card.className} ${
                 currentIndex === index ? "opacity-100" : "opacity-0"
-              } transition-opacity duration-300 object-cover snap-center snap-always`}
+              } transition-opacity duration-300 object-cover`}
             >
               <TarotCard
                 imageSrc={card.imageSrc}
@@ -66,9 +66,52 @@ export default function ServicesBanner() {
               />
             </div>
           ))}
+        </div> */}
+        <div className="flex items-center justify-center">
+          <div
+            className="z-1 transition-all duration-300
+            md:mt-20 md:rotate-[-20deg]
+            lg:mt-30 lg:rotate-[-30deg]
+            xl:mt-40 xl:rotate-[-40deg]
+            "
+          >
+            <TarotCard
+              imageSrc="/servicetarotmark1.png"
+              nameCard="Daily Horoscope Readings"
+              describeCard="Short, engaging predictions for love, career, health, and personal growth Users can select their zodiac sign to view personalized readings."
+            />
+          </div>
+          <div
+            className="z-2 -ml-[370px] transition-all duration-300
+            md:-ml-[300px]
+            lg:-ml-[170px]
+            xl:-ml-[80px]
+            2xl:-ml-[0px]
+            "
+          >
+            <TarotCard
+              imageSrc="/servicetarotmark2.png"
+              nameCard="Natal Birth Chart Analysis"
+              describeCard="Explain the significance of the Sun, Moon, and Rising signs, along with planetary placements Interactive birth chart generator where users input their details."
+            />
+          </div>
+          <div
+            className="z-3 -ml-[370px] transition-all duration-300
+            md:-ml-[300px] md:mt-20 md:rotate-[20deg]
+            lg:-ml-[170px] lg:mt-30 lg:rotate-[30deg]
+            xl:-ml-[80px] xl:mt-40 xl:rotate-[40deg]
+            2xl:-ml-[0px]
+            "
+          >
+            <TarotCard
+              imageSrc="/servicetarotmark3.png"
+              nameCard="Couples Compatibility Report"
+              describeCard="Insights into emotional, intellectual, and physical compatibility. Users enter their birth details to receive a compatibility score and analysis."
+            />
+          </div>
         </div>
       </div>
-      <div className="bg-[#E6E6FA] flex flex-col">
+      <div className="bg-[#E6E6FA] flex flex-col snap-end snap-always">
         <p
           className="text-[#1A1A40] w-[30vw] text-center text-base font-bold pt-[10vw] ml-[7vw]
         sm:text-xl
--- src/app/components/WhyChooseUsBanner.tsx
+++ src/app/components/WhyChooseUsBanner.tsx
@@ -2,128 +2,147 @@ import Image from "next/image";
 
 export default function WhyChooseUsBanner() {
   return (
-    <div className="bg-[#1A1A40] snap-start snap-always">
+    <div
+      className="bg-[#1A1A40] snap-start snap-always pb-[10vw]
+    xl:pb-[5vw]"
+    >
       <div
-        className="text-center text-[#ffd700] font-bold text-4xl pt-[20vw] mb-[5vw]
-      lg:text-5xl"
+        className="text-center text-[#ffd700] font-bold text-xl pt-[25vw] mb-[5vw]
+        sm:text-2xl sm:pt-[20vw]
+        md:text-2xl md:pt-[15vw] md:mb-[5vw]
+        lg:text-3xl
+        xl:text-4xl xl:pt-[10vw] xl:mb-[3vw]
+        2xl:text-6xl"
       >
         <p>Why Choose Us?</p>
       </div>
-      <div
-        className="items-center justify-center transition-all duration-300
-      lg:flex lg:ml-[5vw]"
-      >
-        <Image
-          src="/whymark1.png"
-          alt="Why Choose Us mark"
-          width={220}
-          height={0}
-          className="w-[25vw] justify-self-center
+      <div className="xl:flex items-center justify-between
+      lg:ml-[5vw] lg:mr-[5vw]">
+        <div
+          className="items-center justify-center transition-all duration-300
+      lg:flex
+      xl:block xl:w-[33%]"
+        >
+          <Image
+            src="/whymark1.png"
+            alt="Why Choose Us mark"
+            width={220}
+            height={0}
+            className="w-[18vw] justify-self-center
           lg:w-[50vw]
-          xl:w-[40vw]
-          2xl:w-[30vw]"
-        />
-        <div className="text-white text-center ml-[5vw] mr-[5vw]">
-          <p
-            className="text-xl mt-[10vw]
-          sm:text-3xl
-          lg:text-4xl
-          xl:text-5xl
-          2xl:text-6xl"
-          >
-            Expert Guidance with Intuitive Accuracy
-          </p>
-          <p
-            className="text-sm mt-[5vw] pb-[10vw]
-          lg:text-base lg:mt-[2vw]
+          xl:w-[10vw]
+          2xl:w-[10vw]"
+          />
+          <div className="text-white text-center ml-[5vw] mr-[5vw]">
+            <p
+              className="text-base mt-[5vw]
           sm:text-xl
-          md:text-2xl
-          lg:text-3xl"
-          >
-            Our seasoned astrologers and tarot readers are deeply attuned to the
-            cosmic energies, ensuring insightful readings that resonate with
-            your personal journey. With years of experience and an intuitive
-            connection to the cards and stars, we guide you toward clarity and
-            enlightenment
-          </p>
+          md:text-xl
+          lg:text-2xl
+          xl:text-2xl
+          2xl:text-4xl"
+            >
+              Expert Guidance with Intuitive Accuracy
+            </p>
+            <p
+              className="text-sm mt-[2.5vw] mb-[10vw]
+          sm:text-sm
+          md:text-base 
+          lg:text-base lg:mb-[5vw]
+          xl:text-base
+          2xl:text-2xl"
+            >
+              Our seasoned astrologers and tarot readers are deeply attuned to
+              the cosmic energies, ensuring insightful readings that resonate
+              with your personal journey. With years of experience and an
+              intuitive connection to the cards and stars, we guide you toward
+              clarity and enlightenment
+            </p>
+          </div>
         </div>
-      </div>
-      <div
-        className="items-center justify-center transition-all duration-300
-      lg:flex lg:ml-[5vw]"
-      >
-        <Image
-          src="/whymark2.png"
-          alt="Why Choose Us mark"
-          width={220}
-          height={0}
-          className="w-[25vw] justify-self-center
+        <div
+          className="items-center justify-center transition-all duration-300
+      lg:flex
+      xl:block xl:w-[33%]"
+        >
+          <Image
+            src="/whymark2.png"
+            alt="Why Choose Us mark"
+            width={220}
+            height={0}
+            className="w-[18vw] justify-self-center
           lg:w-[50vw]
-          xl:w-[40vw]
+          xl:w-[10vw]
           2xl:w-[30vw]"
-        />
-        <div className="text-white text-center ml-[5vw] mr-[5vw]">
-          <p
-            className="text-xl mt-[10vw]
-          sm:text-3xl
-          lg:text-4xl
-          xl:text-5xl
-          2xl:text-6xl"
-          >
-            Personalized & Empowering Readings
-          </p>
-          <p
-            className="text-sm mt-[5vw] pb-[10vw]
-          lg:text-base lg:mt-[2vw]
+          />
+          <div className="text-white text-center ml-[5vw] mr-[5vw]">
+            <p
+              className="text-base mt-[5vw]
           sm:text-xl
-          md:text-2xl
-          lg:text-3xl"
-          >
-            We believe fate is a path, not a fixed destination. Every reading is
-            tailored to your unique questions and concerns, helping you make
-            informed choices rather than simply predicting outcomes. Our
-            approach empowers you to embrace your destiny with confidence and
-            wisdom.
-          </p>
+          md:text-xl
+          lg:text-2xl
+          xl:text-2xl
+          2xl:text-4xl"
+            >
+              Personalized & Empowering Readings
+            </p>
+            <p
+              className="text-sm mt-[2.5vw] mb-[10vw]
+          sm:text-sm
+          md:text-base 
+          lg:text-base lg:mb-[5vw]
+          xl:text-base
+          2xl:text-2xl"
+            >
+              We believe fate is a path, not a fixed destination. Every reading
+              is tailored to your unique questions and concerns, helping you
+              make informed choices rather than simply predicting outcomes. Our
+              approach empowers you to embrace your destiny with confidence and
+              wisdom.
+            </p>
+          </div>
         </div>
-      </div>
-      <div
-        className="items-center justify-center transition-all duration-300
-      lg:flex lg:ml-[5vw]"
-      >
-        <Image
-          src="/whymark3.png"
-          alt="Why Choose Us mark"
-          width={220}
-          height={0}
-          className="w-[25vw] justify-self-center
+        <div
+          className="items-center justify-center transition-all duration-300
+      lg:flex
+      xl:block xl:w-[33%]"
+        >
+          <Image
+            src="/whymark3.png"
+            alt="Why Choose Us mark"
+            width={220}
+            height={0}
+            className="w-[18vw] justify-self-center
           lg:w-[50vw]
-          xl:w-[40vw]
+          xl:w-[10vw]
           2xl:w-[30vw]"
-        />
-        <div className="text-white text-center ml-[5vw] mr-[5vw]">
-          <p
-            className="text-xl mt-[10vw]
-          sm:text-3xl
-          lg:text-4xl
-          xl:text-5xl
-          2xl:text-6xl"
-          >
-            Safe and Transformative Experience
-          </p>
-          <p
-            className="text-sm mt-[5vw] pb-[10vw]
-          lg:text-base lg:mt-[2vw]
+          />
+          <div className="text-white text-center ml-[5vw] mr-[5vw]">
+            <p
+              className="text-base mt-[5vw]
           sm:text-xl
-          md:text-2xl
-          lg:text-3xl"
-          >
-            Whether you are seeking clarity, healing, or direction, we provide a
-            welcoming and supportive space for your spiritual journey. With a
-            focus on positivity and self-discovery, our readings are designed to
-            inspire, soothe, and uplift.and self-discovery and self-discovery
-            and self-discovery
-          </p>
+          md:text-xl
+          lg:text-2xl
+          xl:text-2xl
+          2xl:text-4xl"
+            >
+              Safe and Transformative Experience
+            </p>
+            <p
+              className="text-sm mt-[2.5vw] mb-[10vw]
+          sm:text-sm
+          md:text-base 
+          lg:text-base lg:mb-[5vw]
+          xl:text-base
+          2xl:text-2xl"
+            >
+              Whether you are seeking clarity, healing, or direction, we provide
+              a welcoming and supportive space for your spiritual journey. With
+              a focus on positivity and self-discovery, our readings are
+              designed to inspire, soothe, and uplift.and self-discovery and
+              self-discovery and self-discovery
+            </p>
+          </div>
         </div>
       </div>
     </div>
